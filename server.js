d7f8409b4f8ef26630e32ca5e070a5ed22e5e853
mgrillex <mohammed30999166@f-eng.tanta.edu.eg>
2025-04-27 21:27:20 +0300

    this is the first commit

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
-const express = require("express");
-false;
+const app = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const courseRoutes = require("./routes/postRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.use("/api/users", userRoutes);
+app.use("/api/courses", courseRoutes);
+app.use("/api/categories", categoryRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
